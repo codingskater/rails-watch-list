a42fc24034c5f46e918ef9e5c82666a460c3acc3
Anna Costello <271207945+codingskater@users.noreply.github.com>
2026-05-15 16:43:19 +0900

    alphabetized movies correctly

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -13,6 +13,7 @@ class BookmarksController < ApplicationController
     if @bookmark.save
       redirect_to list_path(@list)
     else
+      @movies = Movie.order(title: :asc)
       render :new, status: :unprocessable_entity
     end
   end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,6 +7,7 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
     @bookmark = Bookmark.new
+    @movies = Movie.order(title: :asc)
   end
 
   def new
